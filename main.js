5c955d42f39db59a1530105edb252f2d671d034d
oipi <tuberosumgames@gmail.com>
2024-12-30 15:29:46 +0530

    test1

--- main.js
+++ main.js
@@ -1,14 +1,22 @@
-const { Client, Intents } = require('discord.js');
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+const { Client, Intents } = require('discord.js')
+require('dotenv').config()
+const client = new Client({ 
+    intents: [
+        Intents.FLAGS.GUILDS, 
+        Intents.FLAGS.GUILD_MESSAGES,
+    ]
+})
+
+const TOKEN = process.env.DISCORD_TOKEN
 
 client.once('ready', () => {
     console.log('URSD-BOT is now online');
 });
 client.on("message", message => {
-    if(message.content === "ursd") {
-        message.channel.send("forever")
+    if(message.content === "negev") {
+        message.channel.send("oy ve")
     }
 });
 
 
-client.login("sample token")
+client.login(TOKEN)
