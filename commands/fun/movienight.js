7bf68328a1f5176f09b1a86bb3d674e593b4afea
oipi <tuberosumgames@gmail.com>
2026-04-25 01:46:50 +0530

    movie night rules

--- commands/fun/movienight.js
+++ commands/fun/movienight.js
@@ -5,6 +5,6 @@ module.exports = {
         .setName('movie-night')
         .setDescription('uri attack'),
     async execute(interaction) {
-        await interaction.reply(process.env.MOVIE_NIGHT);
+        await interaction.reply(`REMINDER: No movie night will take place if movie is started after 22:00 IST. \n\n${process.env.MOVIE_NIGHT}`);
     },
 };
