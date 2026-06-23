2f501e21e708df91dcb593964b5808f7f0cae6a0
oipi <tuberosumgames@gmail.com>
2026-06-24 00:29:20 +0530

    fixed audio issues

--- commands/fun/igotthis.js
+++ commands/fun/igotthis.js
@@ -6,9 +6,16 @@ module.exports = {
         .setName('igotthis')
         .setDescription('death note bgm'),
     async execute(interaction) {
-        const filePath = path.join(__dirname, '..', '..', 'attachments', 'igotthis.mp3');
-        const file = new AttachmentBuilder(filePath);
-        
-        await interaction.reply({files:[file]});
+        await interaction.deferReply();
+        try {
+            const filePath = path.join(__dirname, '..', '..', 'attachments', 'igotthis.mp3');
+            const file = new AttachmentBuilder(filePath);
+            
+            await interaction.editReply({ files: [file] });
+            
+        } catch (error) {
+            console.error('Error sending file:', error);
+            await interaction.editReply({ content: 'Sorry, I ran into an issue uploading the file!' });
+        }
     },
 };
